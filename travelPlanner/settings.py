3d60f4a09f3942ec38d173c967ba5f7da05dc07c
kiruthihan nagarajan <kiruthihan10@gmail.com>
2025-03-09 19:44:10 +0530

    Add conditional database configuration based on user domain

--- travelPlanner/settings.py
+++ travelPlanner/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,12 +74,25 @@ WSGI_APPLICATION = "travelPlanner.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+userDomain = os.getenv("USERDOMAIN")
+
+if userDomain == "GHOSTMACHINE":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "TravelPlan",
+            "USER": "postgres",
+            "PASSWORD": "abc123456;",
+            "HOST": "localhost",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
